ee2f06bbfe2282afde9a49c338a8fbfff149a0d8
zhakiya-00 <zakiyayusuf123@gmail.com>
2022-11-16 12:22:33 -0800

    firsttask

---
+++ shell.h
@@ -0,0 +1,25 @@
+#ifndef SHELL_H
+#define  SHELL_H
+
+#include <stdio.h>
+
+#include <string.h>
+
+#include <stdlib.h>
+
+#include <unistd.h>
+
+#include <sys/wait.h>
+
+#include <sys/types.h>
+
+#include <sys/stat.h>
+
+extern char **environ;
+
+char *_getenv(const char *name);
+
+char *_printpath();
+
+char *get_cmd(char *name);
+#endif
